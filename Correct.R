c6ab741001ebabd6cee1c124ca6cb81c808b6d6f
sophiapalcic <107637253+sophiapalcic@users.noreply.github.com>
2022-06-21 17:08:05 -0400

    Correct function for IC

---
+++ Correct.R
@@ -0,0 +1,66 @@
+###Function to know if the IC chose correctly
+
+#thumb = variable to use for "rule of thumb"
+
+# 1 = Success
+# 0 = Fail
+
+correct <- function(exp_col_num_AIC, diff_matrix, thumb) {
+  #correct_matrix = matrix(nrow = N, ncol = 1)
+  
+  N = dim(diff_matrix)[1]
+  exp_col_num_AICc = exp_col_num_AIC + 1
+  exp_col_num_BIC = exp_col_num_AIC + 2
+  
+  for (i in 1:N) {
+    ##Failures and successes for AIC
+    for (j in seq(18, from = 1, by = 3)) {
+      if (i == exp_col_num_AIC) {
+        
+      }
+      else{
+        if (diff_matrix[i, j] > thumb) {
+          print(1)
+        }
+        else{
+          print(0)
+        }
+      }
+    }
+  }
+  
+  #Failure and successes for AICc
+  
+  for (j in seq(18, from = 1, by = 3)) {
+    if (i == exp_col_num_AICc) {
+      
+    }
+    else{
+      if (diff_matrix[i, j] > thumb) {
+        print(1)
+      }
+      else{
+        print(0)
+      }
+    }
+  }
+  
+  
+  #Failure and successes for BIC
+  for (j in seq(18, from = 3, by = 3)) {
+    if (i == exp_col_num_BIC) {
+      
+    }
+    else{
+      if (diff_matrix[i, j] > thumb) {
+        print(1)
+      }
+      else{
+        print(0)
+      }
+    }
+  }
+}
+
+
+
