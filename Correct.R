07f4ce4ffa8571a9a55cc2dc55c72587b5dd1660
deckermh <54589995+deckermh@users.noreply.github.com>
2022-06-22 09:56:28 -0400

    reviewed correct.R

--- Correct.R
+++ Correct.R
@@ -6,16 +6,47 @@
 # 0 = Fail
 
 correct <- function(exp_col_num_AIC, diff_matrix, thumb) {
-  #correct_matrix = matrix(nrow = N, ncol = 1)
-  
   N = dim(diff_matrix)[1]
+  
   exp_col_num_AICc = exp_col_num_AIC + 1
   exp_col_num_BIC = exp_col_num_AIC + 2
   
   for (i in 1:N) {
     ##Failures and successes for AIC
     for (j in seq(18, from = 1, by = 3)) {
-      if (i == exp_col_num_AIC) {
+      if (j == exp_col_num_AIC) {
+        
+      }
+      else{
+        if (diff_matrix[i, j] > thumb) {
+          print(1)
+        }
+        else{
+          print(0)
+        }
+      }
+    }
+    
+    #Failure and successes for AICc
+    
+    for (j in seq(18, from = 2, by = 3)) {
+      if (j == exp_col_num_AICc) {
+        
+      }
+      else{
+        if (diff_matrix[i, j] > thumb) {
+          print(1)
+        }
+        else{
+          print(0)
+        }
+      }
+    }
+    
+    
+    #Failure and successes for BIC
+    for (j in seq(18, from = 3, by = 3)) {
+      if (j == exp_col_num_BIC) {
         
       }
       else{
@@ -28,39 +59,4 @@ correct <- function(exp_col_num_AIC, diff_matrix, thumb) {
       }
     }
   }
-  
-  #Failure and successes for AICc
-  
-  for (j in seq(18, from = 1, by = 3)) {
-    if (i == exp_col_num_AICc) {
-      
-    }
-    else{
-      if (diff_matrix[i, j] > thumb) {
-        print(1)
-      }
-      else{
-        print(0)
-      }
-    }
-  }
-  
-  
-  #Failure and successes for BIC
-  for (j in seq(18, from = 3, by = 3)) {
-    if (i == exp_col_num_BIC) {
-      
-    }
-    else{
-      if (diff_matrix[i, j] > thumb) {
-        print(1)
-      }
-      else{
-        print(0)
-      }
-    }
-  }
 }
-
-
-
